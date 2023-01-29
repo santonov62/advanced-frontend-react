f8368cabbc9ce639b9a2a3cedcd19a6c46b62d07
santonov <santonov@nymbus.com>
2023-01-29 21:26:09 +0300

    Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+    mode: "development",
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve(__dirname, 'build'),
+        clean: true
+    },
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')}),
+    ]
+};
