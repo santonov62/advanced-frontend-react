a87014502656fb2597d1ab424757afa9deca5dc0
santonov <santonov@nymbus.com>
2023-01-30 17:29:08 +0300

    Sass support

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,17 @@
 import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
+    const sassLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+        ],
+    };
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -8,5 +19,6 @@ export function buildLoaders(): webpack.RuleSetRule[] {
     };
     return [
         typescriptLoader,
+        sassLoader,
     ];
 }
