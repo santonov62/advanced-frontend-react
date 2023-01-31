cf7259c0be2a81c9ddef91f9fe19a90353c39e3d
santonov <santonov@nymbus.com>
2023-01-31 13:15:01 +0300

    React routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import {Counter} from "./components/Counter";
+import {Link, Route, Routes } from 'react-router-dom';
 import './index.scss';
+import AboutPage from "./pages/AboutPage/AboutPage";
+import MainPage from "./pages/MainPage/MainPage";
 
 const App = () => {
     return (
         <div className="app">
-            asdasdasdasdasdfasf
-            <Counter />
+            <Link to={'/'}>Главная</Link>
+            <Link to={'about'}>О сайте</Link>
+            <Routes>
+                <Route path={'/about'} element={<AboutPage />}/>
+                <Route path={'/'} element={<MainPage />}/>
+            </Routes>
         </div>
     );
 };
