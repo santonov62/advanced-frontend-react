3f90cc03cd2c591f552fa469ccc8e17a08e15eca
santonov <santonov@nymbus.com>
2023-02-09 22:43:12 +0300

    Eslint support

--- src/shared/ui/LanguageSwitcher/ui/LanguageSwitcher.tsx
+++ src/shared/ui/LanguageSwitcher/ui/LanguageSwitcher.tsx
@@ -1,7 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
 import { useTranslation } from 'react-i18next';
-import cls from './LanguageSwitcher.module.scss';
 
 interface LanguageSwitcherProps {
     className?: string
@@ -15,7 +14,7 @@ export const LanguageSwitcher = ({ className }: LanguageSwitcherProps) => {
     };
     return (
         <Button
-            className={classNames(cls.languageSwitcher, {}, [className])}
+            className={classNames('', {}, [className])}
             theme={ThemeButton.CLEAR}
             onClick={toggle}
         >
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(cls.navbar, {}, [className])}>
-        <div style={{ flexGrow: 1 }} />
-        <AppLink theme={AppLinkTheme.SECONDARY} to="/">Главная</AppLink>
-        <AppLink theme={AppLinkTheme.IMPORTANT} to="about">О сайте</AppLink>
-    </div>
-);
+export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
+    return (
+        <div className={classNames(cls.navbar, {}, [className])}>
+            <div style={{ flexGrow: 1 }} />
+            <AppLink theme={AppLinkTheme.SECONDARY} to="/">{t('mainPage')}</AppLink>
+            <AppLink theme={AppLinkTheme.IMPORTANT} to="about">{t('about')}</AppLink>
+        </div>
+    );
+};
