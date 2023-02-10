25db8564bde934853f8b472ccc51a77b488827ab
santonov <santonov@nymbus.com>
2023-02-10 18:58:48 +0300

    Jest support

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,43 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with only first param', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+
+    test('with additional class', () => {
+        const expected = 'someClass class1 class2';
+        expect(classNames('someClass', {}, ['class1', 'class2']))
+            .toBe(expected);
+    });
+
+    test('with mods', () => {
+        const expected = 'someClass class1 class2 class3 class4';
+        expect(classNames(
+            'someClass',
+            { class3: true, class4: true },
+            ['class1', 'class2'],
+        ))
+            .toBe(expected);
+    });
+
+    test('with mods false', () => {
+        const expected = 'someClass class1 class2 class3';
+        expect(classNames(
+            'someClass',
+            { class3: true, class4: false },
+            ['class1', 'class2'],
+        ))
+            .toBe(expected);
+    });
+
+    test('with mods undefined', () => {
+        const expected = 'someClass class1 class2 class3';
+        expect(classNames(
+            'someClass',
+            { class3: true, class4: undefined },
+            ['class1', 'class2'],
+        ))
+            .toBe(expected);
+    });
+});
