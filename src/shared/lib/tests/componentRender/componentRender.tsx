98ebc04c7fd3b195b0dd25a95b2550a29556236c
santonov <santonov@nymbus.com>
2023-02-19 22:51:12 +0300

    Fix tests

---
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -0,0 +1,22 @@
+import { render } from '@testing-library/react';
+import { ReactNode } from 'react';
+import { I18nextProvider } from 'react-i18next';
+import i18n from 'shared/config/i18n/i18nForTests';
+import { MemoryRouter } from 'react-router-dom';
+
+interface ComponentRenderProps {
+    route?: string
+}
+
+export function componentRender(component: ReactNode, options: ComponentRenderProps = {}) {
+    const {
+        route = '/',
+    } = options;
+    return render(
+        <MemoryRouter initialEntries={[route]}>
+            <I18nextProvider i18n={i18n}>
+                {component}
+            </I18nextProvider>
+        </MemoryRouter>,
+    );
+}
