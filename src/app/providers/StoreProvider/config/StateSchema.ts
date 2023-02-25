910145c1ad36131f6c4dc399360a811173c696d8
santonov <santonov@nymbus.com>
2023-02-25 21:24:06 +0300

    Try to find error #11

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -1,4 +1,4 @@
-import { CounterSchema } from 'entities/Counter/model/types/CounterSchema';
+import { CounterSchema } from 'entities/Counter';
 
 export interface StateSchema {
     counter: CounterSchema
