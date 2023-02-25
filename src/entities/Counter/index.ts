301e719f853895084f206c28292bd6abfaf1efa6
santonov <santonov@nymbus.com>
2023-02-25 19:50:44 +0300

    Try to find error #3

--- src/entities/Counter/index.ts
+++ src/entities/Counter/index.ts
@@ -1,3 +1,3 @@
 export { CounterReducer, CounterActions } from './model/slice/CounterSlice';
 export { CounterSchema } from './model/types/CounterSchema';
-// export { Counter } from './ui/Counter';
+export { Counter } from './ui/Counter';
