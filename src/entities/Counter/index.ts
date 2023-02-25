7222fc6a3dd40133a3a43fb6ac7d9b7e8b067752
santonov <santonov@nymbus.com>
2023-02-25 22:05:53 +0300

    Try to find import error

--- src/entities/Counter/index.ts
+++ src/entities/Counter/index.ts
@@ -1,3 +1,4 @@
-export { CounterReducer, CounterActions } from './model/slice/CounterSlice';
+export { CounterReducer } from './model/slice/CounterSlice';
+export { CounterActions } from './model/slice/CounterSlice';
 export { CounterSchema } from './model/types/CounterSchema';
 export { Counter } from './ui/Counter';
