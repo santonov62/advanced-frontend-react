ede9ad5799d8e369399297989bcc39818ec3855b
santonov <santonov@nymbus.com>
2023-02-26 16:13:34 +0300

    Try to fix import error #1

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -47,7 +47,6 @@ export default {
     moduleNameMapper: {
         '\\.s?css$': 'identity-obj-proxy',
         '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
-        '^@/(.*)$': '<rootDir>/src/$1',
     },
 
     globals: {
