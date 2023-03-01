dd53556be009f54c752471056839d334035f62eb
santonov <santonov@nymbus.com>
2023-03-01 11:36:26 +0300

    User entity

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,12 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { CounterReducer } from 'entities/Counter';
+import { configureStore, ReducersMapObject } from '@reduxjs/toolkit';
+import { counterReducer } from 'entities/Counter';
+import { userReducer } from 'entities/User';
 import { StateSchema } from './StateSchema';
 
 export function createReduxStore(initialState?: StateSchema) {
+    const rootReducer: ReducersMapObject<StateSchema> = {
+        counter: counterReducer,
+        user: userReducer,
+    };
+
     return configureStore<StateSchema>({
-        reducer: {
-            counter: CounterReducer,
-        },
+        reducer: rootReducer,
         devTools: __IS_DEV__,
         preloadedState: initialState,
     });
