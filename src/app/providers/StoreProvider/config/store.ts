c4fcdb20a0c34769e7415296f8db82a303550dd6
santonov <santonov@nymbus.com>
2024-03-05 14:55:17 +0300

    api, navigate into extra of thunk

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -3,11 +3,14 @@ import { counterReducer } from 'entities/Counter';
 import { userReducer } from 'entities/User';
 import { createReducerManager } from 'app/providers/StoreProvider/config/ReduceManager';
 import { $api } from 'shared/api/api';
+import { NavigateOptions } from 'react-router';
+import { To } from 'history';
 import { StateSchema } from './StateSchema';
 
 export function createReduxStore(
     initialState?: StateSchema,
     asyncReducers?: ReducersMapObject<StateSchema>,
+    navigate?: (to: To, options?: NavigateOptions) => void,
 ) {
     const rootReducer: ReducersMapObject<StateSchema> = {
         ...asyncReducers,
@@ -25,6 +28,7 @@ export function createReduxStore(
             thunk: {
                 extraArgument: {
                     api: $api,
+                    navigate,
                 },
             },
         }),
--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -1,19 +1,19 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { User, userActions } from 'entities/User';
 import { USER_LOACALSTORAGE_KEY } from 'shared/const/localstorage';
+import { ThunkConfig } from 'app/providers/StoreProvider';
 
 interface LoginByUsernameProps {
     username: string;
     password: string;
 }
 
-export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, {rejectValue: string}>(
+export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, ThunkConfig<string>>(
     'login/loginByUsername',
-    async ({ username, password }, { dispatch, extra, rejectWithValue }) => {
+    async ({ username, password }, thunkApi) => {
+        const { dispatch, extra, rejectWithValue } = thunkApi;
         try {
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            const response = await extra.api.post<User>('http://localhost:8000/login', {
+            const response = await extra.api.post<User>('/login', {
                 username, password,
             });
             const { data } = response;
